c0b0b9994d6970f9e6a1fa99d3d6b9c7bdef90c2
Amireza Fatemi <arfo.amireza@gmail.com>
2017-01-26 21:14:09 +0100

    Some refrence

--- run.rb
+++ run.rb
@@ -12,3 +12,13 @@ g = Git.open(working_dir, :log => Logger.new(STDOUT))
 a = %x(git log)
 
 p a
+
+
+# Refrence
+## Biggest files:
+##  $ find src -type f | xargs wc -l | sort | tail
+##  $ find src -type f | xargs cloc -by-file | head -n 20
+
+## Most changed
+##  $ git log --pretty=format: --name-only -- src | sort | uniq -c | sort -rg | head
+##  $ git log --pretty=format: --name-only --after="1 year ago"  -- src | sort | uniq -c | sort -rg | head
