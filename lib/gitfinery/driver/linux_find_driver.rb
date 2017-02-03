ab4786206b422c29dffe4517f92bf6db3c2799b4
Amireza Fatemi <arfo.amireza@gmail.com>
2017-02-03 21:49:05 +0100

    Add find method to find driver
    find files and folders based on provided directory, the defauly dir is
    pwd

--- lib/gitfinery/driver/linux_find_driver.rb
+++ lib/gitfinery/driver/linux_find_driver.rb
@@ -1,6 +1,11 @@
 module Gitfinery
   module Driver
     class LinuxFindDriver
+      def find dir = '.'
+        return Hash[ %x(find #{dir})
+          .split("\n")
+          .map { |l| l.chomp.split(' ',2) } ]
+      end
     end
   end
 end
--- test/driver/linux_find_driver_test.rb
+++ test/driver/linux_find_driver_test.rb
@@ -1,9 +1,26 @@
 require_relative '../test_base'
 
 class TestLinuxFindDriver < TestBase
+  def setup
+    @resource_dir = "#{Dir.pwd}/test/driver/resource"
+  end
+  
+
   def test_class_exisit
     assert_nothing_raised "linux find driver dosn't exists!" do
       Gitfinery::Driver::LinuxFindDriver.new
     end
+  end
+
+  def test_call_find_method
+    assert_nothing_raised "Cannot call method find!" do
+      Gitfinery::Driver::LinuxFindDriver.new.find
+    end
+  end
+
+  def test_call_find_retruns_all_files_in_resource_folder
+    files = Gitfinery::Driver::LinuxFindDriver.new.find(@resource_dir)
+
+    assert_equal(4, files.count, 'The number of files returned is not match with actual one')
   end
 end
