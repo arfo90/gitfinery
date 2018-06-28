fd0d2473758a38e5c4344dd941f06f81ecc5075d
Amireza Fatemi <arfo.amireza@gmail.com>
2018-06-28 19:15:44 +0200

    Add suppourt for getting top changed files via git

--- lib/gitfinery/driver/git.rb
+++ lib/gitfinery/driver/git.rb
@@ -3,7 +3,25 @@ require 'git'
 module Gitfinery
   module Driver
     class Git
+      def initialize(dir = '.')
+        @dir = dir
+      end
+
       def self.log
+      end
+
+      def most_changed
+        files = %x(git --git-dir=#{@dir}/.git/ log --pretty=format: --name-only | sort | uniq -c | sort -rg | head -10
+        ).split("\n")
+
+        targets = []
+        list = []
+
+        files.each do |f|
+          target = f.split(" ")
+          targets << { name: target[1], total_change: target[0] } unless target[1].nil?
+        end
+        targets
       end
     end
   end
--- test/driver/git_test.rb
+++ test/driver/git_test.rb
@@ -10,5 +10,14 @@ class TestGit < TestBase
       Gitfinery::Driver::Git.new
     end
   end
+
+  def test_return_most_changed_targets
+    log = Gitfinery::Driver::Git.new("#{Dir.pwd}/test/driver/git_mock").most_changed
+    assert_equal([{:name=>"target1", :total_change=>"4"}, {:name=>"target2", :total_change=>"1"}],log, "failed to return most changed files")
+  end
 end
+
+
+
+
 
